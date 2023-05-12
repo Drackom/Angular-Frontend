c1342cadc581edac71668c60a9f4f97a9372e1fa
Drackom <sergiourielquinteros@gmail.com>
2023-05-11 21:31:35 -0300

    Actualizacion

---
+++ Uriel-Portfolio/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { PortadaComponent } from './componentes/portada/portada.component';
+import { InformacionComponent } from './componentes/informacion/informacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EstudiosComponent } from './componentes/estudios/estudios.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { HttpClientModule } from '@angular/common/http';
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent,
+    PortadaComponent,
+    InformacionComponent,
+    ExperienciaComponent,
+    EstudiosComponent,
+    HabilidadesComponent,
+    ProyectosComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Uriel-Portfolio/src/app/componentes/informacion/informacion.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
+
+@Component({
+  selector: 'app-informacion',
+  templateUrl: './informacion.component.html',
+  styleUrls: ['./informacion.component.css']
+})
+export class InformacionComponent implements OnInit {
+  miPortfolio:any;
+  constructor(private datosPortfolio:PortfolioService){}
+
+  ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data =>{
+      console.log(data);
+      this.miPortfolio=data;
+    });
+  }
+}
